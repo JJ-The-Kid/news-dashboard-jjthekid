7cd76bd2c5cad901ce3dce02fc666c4984e5b760
ga95jip <julian.seyfarth@tum.de>
2020-10-22 16:31:24 +0200

    postgres configuration added

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,21 @@
-require('dotenv').config();
-
-module.exports = {
-    client: 'mysql',
-    connection: {
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE
-    },
-    pool: { min: 1, max: 100 }
-};
+if (process.env.DATABASE_URL) {
+    // running on Heroku using Postgres
+    module.exports = {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        searchPath: ['knex', 'public'],
+        pool: { min: 1, max: 100 }
+    };
+} else {
+    // running locally using MySQL
+    require('dotenv').config();
+    module.exports = {
+        client: 'mysql',
+        connection: {
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE
+        },
+        pool: { min: 1, max: 100 }
+    };
+}
